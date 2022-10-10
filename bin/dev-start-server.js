5f4ed4a91c6d9c4c3a6160691d1819117e144060
Jared Grippe <jared@deadlyicon.com>
2022-10-10 12:18:34 -0700

    WIP

--- bin/dev-start-server.js
+++ bin/dev-start-server.js
@@ -1,15 +1,12 @@
 #!/usr/bin/env node
 
-import Path from 'node:path'
-import { fileURLToPath } from 'url'
-
 process.env.NODE_ENV = 'development'
-process.env.APP_PATH = Path.resolve(process.argv[2])
-process.env.SHARED_PATH = Path.resolve(fileURLToPath(import.meta.url), '../../shared')
 
-console.log('process.env.SHARED_PATH', process.env.SHARED_PATH)
+import { packageDirectory } from 'pkg-dir'
 
 await (async () => {
+  process.env.APP_PATH = await packageDirectory()
+  console.log('process.env.APP_PATH', process.env.APP_PATH)
   const { createServer } = await import('jlinx-demo-app/server/index.js')
   const server = await createServer()
   await server.start()
