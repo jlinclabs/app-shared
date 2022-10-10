be3f6b3a7ce6eb2f9813061ea9a56afd94e68af3
Jared Grippe <jared@deadlyicon.com>
2022-10-10 12:19:35 -0700

    WIP

--- bin/dev-start-server.js
+++ bin/dev-start-server.js
@@ -7,7 +7,7 @@ import { packageDirectory } from 'pkg-dir'
 await (async () => {
   process.env.APP_PATH = await packageDirectory()
   console.log('process.env.APP_PATH', process.env.APP_PATH)
-  const { createServer } = await import('jlinx-demo-app/server/index.js')
+  const { createServer } = await import('../server/index.js')
   const server = await createServer()
   await server.start()
 })()
