881b99fcf9c04b3035f4aad51032775e0b14134c
Jared Grippe <jared@deadlyicon.com>
2022-10-30 16:58:08 -0700

    WIP

---
+++ client-src/hooks/useCountdown.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import useForceUpdate from './useForceUpdate.js'
+
+export default function useCountdown(seconds){
+  const forceUpdate = useForceUpdate()
+  const [endTime] = useState(Date.now() + seconds)
+  const secondsRemaining = Math.ceil((endTime - Date.now()) / 1000)
+  console.log({ seconds, endTime, secondsRemaining })
+  useEffect(
+    () => {
+      const id = setInterval(
+        () => {
+          forceUpdate()
+          if (Date.now() > endTime) { clearInterval(id) }
+        },
+        100
+      )
+      return () => clearInterval(id)
+    },
+    [endTime]
+  )
+  return secondsRemaining
+}
---
+++ client/hooks/useCountdown.js
@@ -0,0 +1,51 @@
+import {useState as $arRGr$useState, useEffect as $arRGr$useEffect, useCallback as $arRGr$useCallback} from "react";
+
+function $parcel$defineInteropFlag(a) {
+  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
+}
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+var $b6a953b0721d86c6$exports = {};
+
+$parcel$defineInteropFlag($b6a953b0721d86c6$exports);
+
+$parcel$export($b6a953b0721d86c6$exports, "default", () => $b6a953b0721d86c6$export$2e2bcd8739ae039);
+
+const $b6a953b0721d86c6$var$noop = ()=>{};
+function $b6a953b0721d86c6$export$2e2bcd8739ae039() {
+    let setState = (0, $arRGr$useState)()[1];
+    (0, $arRGr$useEffect)(()=>()=>{
+            setState = $b6a953b0721d86c6$var$noop;
+        }, []);
+    return (0, $arRGr$useCallback)(()=>{
+        setState({});
+    }, []);
+}
+
+
+function $0c8d6bf565bc2e40$export$2e2bcd8739ae039(seconds) {
+    const forceUpdate = (0, $b6a953b0721d86c6$export$2e2bcd8739ae039)();
+    const [endTime] = (0, $arRGr$useState)(Date.now() + seconds);
+    const secondsRemaining = Math.ceil((endTime - Date.now()) / 1000);
+    console.log({
+        seconds: seconds,
+        endTime: endTime,
+        secondsRemaining: secondsRemaining
+    });
+    useEffect(()=>{
+        const id = setInterval(()=>{
+            forceUpdate();
+            if (Date.now() > endTime) clearInterval(id);
+        }, 100);
+        return ()=>clearInterval(id);
+    }, [
+        endTime
+    ]);
+    return secondsRemaining;
+}
+
+
+export {$0c8d6bf565bc2e40$export$2e2bcd8739ae039 as default};
+//# sourceMappingURL=useCountdown.js.map
